2125879b22d0c86a4c8e14a2f4f3ff276060dc4a
Geonseob Kim <gnksbm@gmail.com>
2024-10-08 14:16:42 +0900

    init

---
+++ SeSacDen/SeSacDenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeSacDenApp.swift
+//  SeSacDen
+//
+//  Created by gnksbm on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SeSacDenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
